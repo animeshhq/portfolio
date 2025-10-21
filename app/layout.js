0c3e6e725fcd0d8da3c31e1daf53198302274c44
Animesh <animeshhq@gmail.com>
2025-10-21 22:24:18 +0530

    Refactor layout.js to include TypeScript types and Script

--- app/layout.js
+++ app/layout.js
@@ -1,26 +1,35 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
-import Provider from "@/app/providers"
+import Provider from "@/app/providers";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Animesh",
-  description: "Hi I am Animesh. I am a creator at heart. Fueled by curiosity and passion for turning ideas into reality",
+  description:
+    "Hi I am Animesh. I am a creator at heart. Fueled by curiosity and passion for turning ideas into reality",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </head>
       <body className={`${inter.className} min-h-fit`}>
         <Provider>
           {children}
           <Navbar />
         </Provider>
+
+        {/* Cloudflare Web Analytics */}
+        <Script
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          strategy="afterInteractive"
+          data-cf-beacon='{"token": "7f40528c1ccd4af1805e86b865ec7b47"}'
+        />
       </body>
     </html>
   );
